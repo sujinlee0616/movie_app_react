e94e125c32457dfa51c0cb94c6be41b10a6d1cae
Sujin Lee <sjnu21@naver.com>
2020-03-05 04:52:39 +0900

    Updates

---
+++ precache-manifest.37088f4b20549caf3e6075e637ec151d.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "94bce7f6f9633d4893d1955ce22094f1",
+    "url": "/movie_app_react/index.html"
+  },
+  {
+    "revision": "a4b9db0f8229326f22e5",
+    "url": "/movie_app_react/static/css/main.c98fc6ab.chunk.css"
+  },
+  {
+    "revision": "cdb44364a8e9463d1386",
+    "url": "/movie_app_react/static/js/2.25717646.chunk.js"
+  },
+  {
+    "revision": "5356fa2f66e46e6c05e4cbe319ac7f1d",
+    "url": "/movie_app_react/static/js/2.25717646.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a4b9db0f8229326f22e5",
+    "url": "/movie_app_react/static/js/main.be0d6d76.chunk.js"
+  },
+  {
+    "revision": "9678b2b7e32aa6f11619",
+    "url": "/movie_app_react/static/js/runtime-main.4c685b15.js"
+  }
+]);
